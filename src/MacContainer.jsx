f940306052c944ae7f344221b8d7fb7ea8c6b3fb
thedevelopersDeveloper <ashirwaddwi5510@gmail.com>
2024-09-01 06:48:25 +0000

    second

--- src/MacContainer.jsx
+++ src/MacContainer.jsx
@@ -1,11 +1,9 @@
+import { useGLTF } from '@react-three/drei'
 import React from 'react'
 
 const MacContainer = () => {
-  return (
-    <mesh>
-        <boxGeometry />
-    </mesh>
-  )
+  let model = useGLTF('./mac.glb')
+  return <primitive object={model.scene} />
 }
 
 export default MacContainer
